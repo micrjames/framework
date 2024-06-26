cfbc42c1c51bcd7d7dbcc5c4fb57d9ba0a0bca17
Michael James <micrjames921@gmail.com>
2024-06-25 22:31:46 -0700

    moved 'container' and 'document' definitions to 'incs' file

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,19 +1,7 @@
-import jsdom from "jsdom";                                                     
-const { JSDOM } = jsdom;
-require('@testing-library/jest-dom');
-import fs from 'fs';
-import path from 'path';
+import { container, document } from "./incs";
 
 describe("", () => {
-   let dom: JSDOM;                                                           
-   let document: Document;
-   let container: Element;
    beforeAll(() => {
-	  const html = fs.readFileSync(path.resolve(__dirname, "..", "src", "template.html"), 'utf8');
-      dom = new JSDOM(html, {runScripts: 'dangerously'});
-      document = dom.window.document;
-      const body = document.body;
-      container = body.firstElementChild;
    });
    describe("", () => {
 	  test.todo("");
